df2c682a8578726e46386854c6cce7f1ef4cbb9f
HongSJae <gtw030488@gmail.com>
2024-04-26 13:59:31 +0900

    🎉 :: project init

---
+++ GitRank-iOS/GitRank-iOS/GitRank_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitRank_iOSApp.swift
+//  GitRank-iOS
+//
+//  Created by 홍승재 on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GitRank_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
