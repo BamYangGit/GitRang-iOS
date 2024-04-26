67f51d49ac7edc3f4a8856e12123277ce2640837
HongSJae <gtw030488@gmail.com>
2024-04-26 14:00:44 +0900

    ✨ :: Clean Architecture file init

---
+++ GitRank-iOS/Data/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  GitRank-iOS
+//
+//  Created by 홍승재 on 4/26/24.
+//
+
+import Foundation
---
+++ GitRank-iOS/Domain/Domain.swift
@@ -0,0 +1,8 @@
+//
+//  Domain.swift
+//  GitRank-iOS
+//
+//  Created by 홍승재 on 4/26/24.
+//
+
+import Foundation
