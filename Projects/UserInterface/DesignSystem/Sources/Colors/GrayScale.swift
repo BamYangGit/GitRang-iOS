4a935f6c8cea3bf86a260df736637c2e859d5b11
HongSJae <gtw030488@gmail.com>
2024-06-06 15:41:25 +0900

    ✨ :: Color Setting

---
+++ Projects/UserInterface/DesignSystem/Sources/Colors/GrayScale.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+public extension Color {
+    struct GrayScale { }
+}
+
+public extension Color.GrayScale {
+    static let gray100: Color = DesignSystemAsset.GrayScale.grayScale100.swiftUIColor
+    static let gray200: Color = DesignSystemAsset.GrayScale.grayScale200.swiftUIColor
+    static let gray300: Color = DesignSystemAsset.GrayScale.grayScale300.swiftUIColor
+    static let gray400: Color = DesignSystemAsset.GrayScale.grayScale400.swiftUIColor
+    static let gray500: Color = DesignSystemAsset.GrayScale.grayScale500.swiftUIColor
+    static let gray600: Color = DesignSystemAsset.GrayScale.grayScale600.swiftUIColor
+    static let gray700: Color = DesignSystemAsset.GrayScale.grayScale700.swiftUIColor
+}
