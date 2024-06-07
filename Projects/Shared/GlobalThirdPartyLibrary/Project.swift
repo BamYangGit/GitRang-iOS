42422b8d798b600c73deb8a799b7c5c8f95baf1b
HongSJae <gtw030488@gmail.com>
2024-06-07 22:44:33 +0900

    ✨ :: fix

--- Projects/Shared/GlobalThirdPartyLibrary/Project.swift
+++ Projects/Shared/GlobalThirdPartyLibrary/Project.swift
@@ -5,6 +5,10 @@ import ProjectDescriptionHelpers
 let project = Project.module(
     name: ModulePaths.Shared.GlobalThirdPartyLibrary.rawValue,
     targets: [
-        .implements(module: .shared(.GlobalThirdPartyLibrary), product: .framework, dependencies: [])
+        .implements(module: .shared(.GlobalThirdPartyLibrary), product: .framework, dependencies: [
+            .SPM.CombineMoya,
+            .SPM.Moya,
+            .SPM.Needle
+        ])
     ]
 )
